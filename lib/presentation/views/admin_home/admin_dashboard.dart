f3a9226fdde6584d1d8ffa45d8e3b374affb6934
Vyshnave KN <vyshnave@manvishonline.in>
2025-11-18 15:32:24 +0530

    logout dialog

--- lib/presentation/views/admin_home/admin_dashboard.dart
+++ lib/presentation/views/admin_home/admin_dashboard.dart
@@ -270,23 +270,15 @@ class _AdminDashboardState extends State<AdminDashboard> {
                                                         builder: (context) {
                                                           return GestureDetector(
                                                             onTap: () {
-                                                              WidgetsBinding.instance.addPostFrameCallback((
-                                                                _,
-                                                              ) async {
-                                                                await provider
-                                                                    .getAllCompanies(
-                                                                      context,
+                                                              provider.isGodown
+                                                                  ? context.pushNamed(
+                                                                      AppRouterConst
+                                                                          .godownWiseScreen,
+                                                                    )
+                                                                  : context.pushNamed(
+                                                                      AppRouterConst
+                                                                          .routeWiseScreen,
                                                                     );
-                                                                provider.isGodown
-                                                                    ? context.pushNamed(
-                                                                        AppRouterConst
-                                                                            .godownWiseScreen,
-                                                                      )
-                                                                    : context.pushNamed(
-                                                                        AppRouterConst
-                                                                            .routeWiseScreen,
-                                                                      );
-                                                              });
                                                             },
                                                             child: CircleAvatar(
                                                               backgroundColor:
@@ -414,14 +406,10 @@ class _AdminDashboardState extends State<AdminDashboard> {
                                                                               context.pushNamed(
                                                                                 AppRouterConst.companyCreationScreen,
                                                                                 extra: {
-                                                                      'tabIndex':
-                                                                          0,
-                                                                      'companyData':
-                                                                          selectedCompany,
-                                                                      'isPop':
-                                                                          false,
-                                                                    },
-                                                                                  
+                                                                                  'tabIndex': 0,
+                                                                                  'companyData': selectedCompany,
+                                                                                  'isPop': false,
+                                                                                },
                                                                               );
                                                                             },
                                                                           ),
@@ -452,14 +440,10 @@ class _AdminDashboardState extends State<AdminDashboard> {
                                                                               context.pushNamed(
                                                                                 AppRouterConst.companyCreationScreen,
                                                                                 extra: {
-                                                                      'tabIndex':
-                                                                          2,
-                                                                      'companyData':
-                                                                          selectedCompany,
-                                                                      'isPop':
-                                                                          false,
-                                                                    },
-                                                                                 
+                                                                                  'tabIndex': 2,
+                                                                                  'companyData': selectedCompany,
+                                                                                  'isPop': false,
+                                                                                },
                                                                               );
                                                                             },
                                                                           ),
@@ -522,14 +506,10 @@ class _AdminDashboardState extends State<AdminDashboard> {
                                                                               context.pushNamed(
                                                                                 AppRouterConst.companyCreationScreen,
                                                                                 extra: {
-                                                                      'tabIndex':
-                                                                          1,
-                                                                      'companyData':
-                                                                          selectedCompany,
-                                                                      'isPop':
-                                                                          false,
-                                                                    },
-                                                                              
+                                                                                  'tabIndex': 1,
+                                                                                  'companyData': selectedCompany,
+                                                                                  'isPop': false,
+                                                                                },
                                                                               );
                                                                             },
                                                                           ),
