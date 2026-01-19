aa1386051f6400b43598073de9d8ec981650af3f
Vinsha M Viswanath <98205259+vinshaviswanath@users.noreply.github.com>
2026-01-19 09:50:18 +0530

    loginbytoken null check issu fixed

--- lib/data/data_sources/authentication/login_by_token/login_by_token.dart
+++ lib/data/data_sources/authentication/login_by_token/login_by_token.dart
@@ -5,6 +5,7 @@ import 'package:mpos_beat/core/failures/failures.dart';
 import 'package:mpos_beat/core/param/param_builder.dart';
 import 'package:mpos_beat/core/service/http_client.dart';
 import 'package:mpos_beat/core/utils/extentions.dart';
+import 'package:mpos_beat/core/utils/imports.dart';
 import 'package:mpos_beat/core/utils/typedefs.dart';
 import 'package:mpos_beat/core/utils/urls.dart';
 import 'package:mpos_beat/data/local_db/app_db.dart';
@@ -38,15 +39,22 @@ class LoginByToken {
           final data = LoginResponse.fromJson(response.data);
           final token = data.loginData?.token;
           final customerId = data.loginData?.customerId;
+          final logindata = data.loginData;
           if (token != null && token.isNotEmpty && data.status != 10) {
             await sharedPreferences.setString("token", token);
           }
           await sharedPreferences.setInt("customerId", customerId ?? 0);
 
           // await appDb.into(appDb.users).insert(User.fromJson(response.data));
-          await appDb.registrationDetailDao.clearAll();
-          await appDb.registrationDetailDao.insertUser(data.loginData!);
-          await appDb.registrationDetailDao.printUsers();
+          if (logindata != null) {
+            await appDb.registrationDetailDao.clearAll();
+            await appDb.registrationDetailDao.insertUser(logindata);
+            await appDb.registrationDetailDao.printUsers();
+          } else {
+            // Optional: log or throw custom error
+            debugPrint("loginData is null, skipping DB insert");
+          }
+
           appDb.select(appDb.registrationDetails).watch();
           return data;
         }
